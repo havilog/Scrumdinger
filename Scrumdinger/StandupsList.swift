2ccd4f519a7023de4014dbc87ea40beb998f5b5e
havi.log <havi.log@kakaopaycorp.com>
2023-04-18 20:19:13 +0900

    ContentView -> StandupsList 이름 변경

--- Scrumdinger/StandupsList.swift
+++ Scrumdinger/StandupsList.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct StandupsList: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -19,8 +19,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct StandupsList_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        StandupsList()
     }
 }
