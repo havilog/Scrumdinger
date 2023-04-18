dc80a41bbeee5a5cd08ab0904475fbec18378d4f
havi.log <havi.log@kakaopaycorp.com>
2023-04-18 19:20:01 +0900

    프로젝트 생성

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by havi.log on 2023/04/18.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
