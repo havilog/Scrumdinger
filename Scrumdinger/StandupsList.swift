957fdce5dbfe4de185aeedda74080b091aaf0426
havi.log <havi.log@kakaopaycorp.com>
2023-04-18 20:29:47 +0900

    StandupList 기본 뷰 추가

--- Scrumdinger/StandupsList.swift
+++ Scrumdinger/StandupsList.swift
@@ -8,19 +8,67 @@
 import SwiftUI
 
 struct StandupsList: View {
+    // viewModel
+    let mockStandUp: [Standup] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(self.mockStandUp) { standup in
+                    Button {
+                        // action Binding
+                    } label: {
+                        CardView(standup: standup)
+                    }
+                    .listRowBackground(standup.theme.mainColor)
+                }
+            }
+            // toolbar, navigationTitle, sheet, navigationDestination, alert 구현
+        }
+    }
+}
+
+struct CardView: View {
+    let standup: Standup
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(self.standup.title)
+                .font(.headline)
+            Spacer()
+            HStack {
+                Label("\(self.standup.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label(self.standup.duration.formatted(.units()), systemImage: "clock")
+                    .labelStyle(.trailingIcon)
+            }
+            .font(.caption)
         }
         .padding()
+        .foregroundColor(self.standup.theme.accentColor)
     }
+}
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
+
+extension URL {
+    fileprivate static let standups = Self.documentsDirectory.appending(component: "standups.json")
 }
 
 struct StandupsList_Previews: PreviewProvider {
     static var previews: some View {
         StandupsList()
+            .previewDisplayName("Mocking initial standups")
     }
 }
